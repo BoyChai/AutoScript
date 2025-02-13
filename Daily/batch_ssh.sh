05638db888743f58e56a7eeaebf8f021a5769fe9
BoyChai <1972567225@qq.com>
2025-02-13 18:25:50 +0800

    feat: add batch copy ssh public key script

---
+++ Daily/batch_ssh.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+hosts=("master1-admin" "node1-monitor" "node2-osd")
+
+username="root"
+password="Qwer1234"
+port=22
+timeout=2
+
+# 检查是否安装 expect
+if ! rpm -qa expect &>/dev/null; then
+    echo "正在安装expect..."
+    yum -y install expect >/dev/null
+    # 再次检查是否安装成功
+    if ! rpm -qa expect &>/dev/null; then
+        echo "安装expect失败，请手动安装"
+        exit 1
+    fi
+fi
+for i in "${hosts[@]}"; do
+    {
+        ping -c1 -W1 $i &>/dev/null
+        if [ $? -eq 0 ]; then
+            (
+                timeout $4 /usr/bin/expect <<-EOF
+					spawn ssh-copy-id $username@$i -p $port
+					expect {
+						"yes/no"   {send "yes\r";exp_continue}
+						"password" {send "$password\r"} 	
+					}
+					expect eof
+				EOF
+            ) &>/dev/null
+
+            timeout $timeout ssh $i echo "$i success"
+            if [ $? -ne 0 ]; then
+                # 如果 SSH 连接失败
+                echo "SSH连接失败，请检查主机 $i"
+            fi
+        else
+            # 如果主机不可达
+            echo "主机 $i 不可达"
+        fi
+    }
+done
